180f70c381ca7949c83f997eec5c1d3006dd23ab
John Lawson <john@jwlawson.co.uk>
2020-01-02 15:43:32 +0000

    Provide required production modules

--- setup-bazel.js
+++ setup-bazel.js
@@ -1,26 +1,49 @@
-const tc = require('@actions/tool-cache')
-const core = require('@actions/core')
-const fs = require('fs')
+const tc = require("@actions/tool-cache");
+const core = require("@actions/core");
+const fs = require("fs");
 
-const IS_WINDOWS = process.platform === 'win32'
+const IS_WINDOWS = process.platform === "win32";
+
+const PACKAGE_NAME = "bazel";
 
 function getURL(version) {
-  const version_regex = /VERSION/gi
-  const platform_regex = /PLATFORM/gi
-  const template_url = 'https://github.com/bazelbuild/bazel/releases/download/VERSION/bazel-VERSION-PLATFORM'
+  const version_regex = /VERSION/gi;
+  const platform_regex = /PLATFORM/gi;
+  const template_url =
+    "https://github.com/bazelbuild/bazel/releases/download/VERSION/bazel-VERSION-PLATFORM";
 
   if (IS_WINDOWS) {
-    return template_url.replace(version_regex, version).replace(platform_regex, 'windows-x86_64.exe')
+    return template_url
+      .replace(version_regex, version)
+      .replace(platform_regex, "windows-x86_64.exe");
   } else {
-    return template_url.replace(version_regex, version).replace(platform_regex, 'linux-x86_64')
+    return template_url
+      .replace(version_regex, version)
+      .replace(platform_regex, "linux-x86_64");
   }
 }
 
-exports.addBazelToPath = async function (version) {
-  const binary = await tc.downloadTool(getURL(version))
-  // The downloaded bazel file is not executable by default
-  fs.chmodSync(binary, 0o777)
-  const cached_bin = await tc.cacheFile(binary, 'bazel', 'bazel', version)
-  await core.addPath(cached_bin)
+function getFileName() {
+  if (IS_WINDOWS) {
+    return "bazel.exe";
+  } else {
+    return "bazel";
+  }
 }
 
+async function addBazelToToolCache(version) {
+  const binary = await tc.downloadTool(getURL(version));
+  if (!IS_WINDOWS) {
+    // The downloaded bazel file is not executable by default
+    fs.chmodSync(binary, 0o777);
+  }
+  return await tc.cacheFile(binary, getFileName(), PACKAGE_NAME, version);
+}
+
+exports.addBazelToPath = async function(version) {
+  let tool = tc.find(PACKAGE_NAME, version);
+  if (!tool) {
+    tool = await addBazelToToolCache(version);
+  }
+  await core.addPath(tool);
+};
