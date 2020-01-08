2160356ed48aec722292995b5096b2089c95d442
John Lawson <john@jwlawson.co.uk>
2020-01-08 12:33:09 +0000

    Add support for versions to action

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+import * as core from '@actions/core';
 import * as setup from './setup-bazel.js';
+import * as version from './version.js';
 
-setup.addBazelToPath('1.2.1');
+async function run() {
+  try {
+    const required_version = core.getInput('bazel-version');
+    const all_version_info = await version.getAllVersionInfo();
+    const bazel_version_info = await version.getLatestMatching(
+      required_version,
+      all_version_info
+    );
+    await setup.addBazelToPath(bazel_version_info.name);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+run();
