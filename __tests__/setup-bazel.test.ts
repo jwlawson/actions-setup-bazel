2cc267489dc6106a0b61cf267430cbd35eff1b2e
John Lawson <john@jwlawson.co.uk>
2020-01-12 12:43:27 +0000

    Remove overall test and move to workflow

--- __tests__/setup-bazel.test.ts
+++ __tests__/setup-bazel.test.ts
@@ -1,5 +1,3 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
 const path = require('path');
 const nock = require('nock');
 
@@ -11,23 +9,6 @@ process.env['RUNNER_TOOL_CACHE'] = cachePath;
 
 import * as setup from '../src/setup-bazel';
 import * as vi from '../src/version-info';
-
-test('setup downloads bazel', async () => {
-  const required_version: vi.VersionInfo = {
-    name: '1.2.1',
-    assets: [],
-    url: '',
-    draft: false,
-    prerelease: false
-  };
-  await setup.addBazelToPath(required_version);
-  // As the PATH is modified, have to explicitly set the env for the call to exec
-  const { stdout, stderr } = await exec('bazel --version', {
-    env: process.env
-  });
-
-  expect(stdout).toMatch(/bazel 1.2.1/);
-});
 
 test('', async () => {
   const required_version: vi.VersionInfo = {
