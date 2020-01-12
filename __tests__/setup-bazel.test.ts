9bbd5a9566590068f3901ce9c222d1fb8bffc5c3
John Lawson <john@jwlawson.co.uk>
2020-01-12 14:00:58 +0000

    Add darwin mocks to test

--- __tests__/setup-bazel.test.ts
+++ __tests__/setup-bazel.test.ts
@@ -9,11 +9,23 @@ process.env['RUNNER_TOOL_CACHE'] = cachePath;
 
 import * as setup from '../src/setup-bazel';
 import * as vi from '../src/version-info';
+import * as fs from 'fs';
 
-test('', async () => {
+afterEach(() => {
+  fs.rmdirSync(cachePath, { recursive: true });
+  fs.rmdirSync(tempPath, { recursive: true });
+});
+
+test('Download uses correct platform url', async () => {
   const required_version: vi.VersionInfo = {
     name: '1.2.1',
     assets: [
+      {
+        name: 'bazel-darwin-x86_64',
+        platform: 'darwin',
+        filetype: 'exe',
+        url: 'https://fakeaddress.com/bazel-darwin-x86_64'
+      },
       {
         name: 'bazel-linux-x86_64',
         platform: 'linux',
@@ -32,21 +44,31 @@ test('', async () => {
     prerelease: false
   };
   nock.disableNetConnect();
-  const windows_nock = nock('https://fakeaddress.com')
-    .get('/bazel-windows-x86_64.exe')
-    .reply(200, { bazel_windows: true });
+  const darwin_nock = nock('https://fakeaddress.com')
+    .get('/bazel-darwin-x86_64')
+    .reply(200, { bazel_darwin: true });
   const linux_nock = nock('https://fakeaddress.com')
     .get('/bazel-linux-x86_64')
     .reply(200, { bazel_linux: true });
+  const windows_nock = nock('https://fakeaddress.com')
+    .get('/bazel-windows-x86_64.exe')
+    .reply(200, { bazel_windows: true });
 
   await setup.addBazelToToolCache(required_version);
   if (process.platform === 'win32') {
-    expect(windows_nock.isDone()).toBe(true);
+    expect(darwin_nock.isDone()).toBe(false);
     expect(linux_nock.isDone()).toBe(false);
-  } else {
+    expect(windows_nock.isDone()).toBe(true);
+  } else if (process.platform === 'darwin') {
+    expect(darwin_nock.isDone()).toBe(true);
+    expect(linux_nock.isDone()).toBe(false);
     expect(windows_nock.isDone()).toBe(false);
+  } else if (process.platform === 'linux') {
+    expect(darwin_nock.isDone()).toBe(false);
     expect(linux_nock.isDone()).toBe(true);
+    expect(windows_nock.isDone()).toBe(false);
   }
+
   nock.cleanAll();
   nock.enableNetConnect();
 });
